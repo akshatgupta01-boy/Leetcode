03d6368794fd68e641eaa9aba38c6fbd52eb8a00
Akshat Gupta <akshatgupta151593@gmail.com>
2026-08-25 21:17:59 +0530

    Time: 21 ms (56.22%) | Memory: 146.4 MB (15.41%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -6,13 +6,13 @@ public:
         for(int i=n-1;i>=0;i--){
             int sum = 0;
             dp[i] = INT_MIN;
-            for(int k=0;k<3 && i + k < n;k++){
+            for(int k = 0; k < 3 && i + k < n; k++){
                 sum += stoneValue[i + k];
                 dp[i] = max(dp[i], sum - dp[i + k + 1]);
             }
         }
         if(dp[0] > 0) return "Alice";
-        if(dp[0] < 0)  return "Bob";
+        else if(dp[0] < 0) return "Bob";
         else return "Tie";
     }
 };
