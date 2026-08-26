c91e6c37095f4dd728c4fe28b32c6f70dc0746dc
Akshat Gupta <akshatgupta151593@gmail.com>
2026-08-26 21:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (64.15%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,14 +1,60 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int neg = 0;
-        int pos = 0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] > 0) pos++;
-            else if(nums[i] < 0) neg++;
+    int lb(vector<int>& nums, int target){
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
         }
-        if(pos > neg) return pos;
-        else return neg;
+        return left;
+    }
+    int maximumCount(vector<int>& nums) {
+ 
+        int n = nums.size();
+        int firstNonNeg = lb(nums, 0);
+        int firstPos = lb(nums, 1);
+        int neg = firstNonNeg;
+        int pos = n - firstPos;
+        return max(neg, pos);
 
+        //int n = nums.size();
+        //int left = 0;
+        //int right = n - 1;
+        //int firstNonNegative = n;
+        //while(left <= right){
+        //    int mid = left + (right - left) / 2;
+        //    if(nums[mid] >= 0){
+        //        firstNonNegative = mid;
+        //        right = mid - 1;
+//
+        //    }
+        //    else{
+        //        left = mid + 1;
+        //    }
+        //}
+        //int neg = firstNonNegative;
+        //left = 0;
+        //right = n - 1;
+        //int firstPos = n;
+        //while(left <= right){
+        //    int mid = left + (right - left) / 2;
+        //    if(nums[mid] > 0){
+        //        firstPos = mid;
+        //        right = mid - 1;
+        //    }
+        //    else{
+        //        left = mid + 1;
+//
+        //    }
+        //}
+        //int pos = n - firstPos;
+        //return max(pos, neg);
     }
 };
