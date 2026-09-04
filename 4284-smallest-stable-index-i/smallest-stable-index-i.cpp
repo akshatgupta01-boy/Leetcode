74eebe644edc1a250846a13078ba57659321ccc4
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-04 11:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (85.58%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -5,13 +5,13 @@ public:
         for(int i=0;i<n;i++){
             int f = INT_MIN;
             int s = INT_MAX;
-            for(int j = 0; j <= i; j++){
+            for(int j=0;j<=i;j++){
                 f = max(f, nums[j]);
             }
             for(int j=i;j<n;j++){
                 s = min(s, nums[j]);
             }
-            if(f - s <= k) return i;
+            if(f - s <= k) return i; 
         }
         return -1;
     }
