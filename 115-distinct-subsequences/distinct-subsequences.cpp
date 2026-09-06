3ee4f2572f3ebce68cfd30b2578553e330f2a103
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-06 19:12:16 +0530

    Time: 7 ms (96.20%) | Memory: 9 MB (86.32%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = t.length();
+        vector<unsigned long long> dp(n + 1, 0);
+        dp[0] = 1;
+        for(char c : s){
+            for(int i=n-1;i>=0;i--){
+                if(c == t[i]){
+                    dp[i+1] += dp[i];
+                }
+            }
+        }
+        return dp[n];
+    }
+};
