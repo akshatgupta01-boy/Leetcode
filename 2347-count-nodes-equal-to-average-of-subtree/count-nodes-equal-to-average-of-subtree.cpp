5ef1fed1a0a08073e417caefe16ffdcf5a341d40
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-10 21:52:52 +0530

    Time: 11 ms (27.56%) | Memory: 15.7 MB (64.72%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -13,15 +13,15 @@ class Solution {
 public:
     pair<int, int> traversal(TreeNode* root, int& count){
         if(root == NULL) return {0, 0};
-        auto[leftSum, leftCount] = traversal(root -> left, count);
-        auto[rightSum, rightCount] = traversal(root -> right, count);
+        auto [leftSum, leftCount] = traversal(root -> left, count);
+        auto [rightSum, rightCount] = traversal(root -> right, count);
         int totalSum = leftSum + rightSum + root -> val;
         int totalCount = leftCount + rightCount + 1;
         if(totalSum / totalCount == root -> val) count++;
         return {totalSum, totalCount};
     }
     int averageOfSubtree(TreeNode* root) {
-        int count = 0; 
+        int count = 0;
         traversal(root, count);
         return count;
     }
