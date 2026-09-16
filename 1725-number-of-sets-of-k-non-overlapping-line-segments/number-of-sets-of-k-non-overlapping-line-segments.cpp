ea2d6c1f5e1c8d13bff28ec3a841c3ee454b7c5a
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-16 22:16:33 +0530

    Time: 102 ms (24.77%) | Memory: 145.1 MB (18.22%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int numberOfSets(int n, int k) {
-        const int mod = 1e9 + 7;
+        const int MOD = 1e9 + 7;
         vector<vector<long long>> dp(n + k, vector<long long> (2 * k + 1, 0));
-        for(int i=0;i<n + k;i++){
-            dp[i][0] = 1;
-        }
+        for(int i=0;i<n;i++) dp[i][0] = 1;
         for(int i=1;i<n+k;i++){
             for(int j=1;j<=2*k;j++){
                 dp[i][j] = dp[i-1][j];
-                if(j <= i){
-                    dp[i][j] = (dp[i][j] +  dp[i-1][j-1]) % mod;
-                }
+                if(j <= i) dp[i][j] = (dp[i][j] + dp[i-1][j-1]) % MOD;
             }
         }
         return dp[n + k - 1][2 * k];
