d7a8d9b684a96cdfdda5cef8ea77f92dd9544582
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-17 21:36:37 +0530

    Time: 6 ms (80.15%) | Memory: 88.8 MB (79.38%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
-
         int n = arr.size();
-
-        // best[i] = shortest valid subarray
-        // ending at or before index i
-        vector<int> best(n, INT_MAX);
-
+        int ans = INT_MAX;
         int left = 0;
         int sum = 0;
-        int answer = INT_MAX;
-
-        for(int right = 0; right < n; right++) {
-
+        vector<int> best(n, INT_MAX);
+        for(int right = 0; right < n; right++){
             sum += arr[right];
-
-            while(sum > target) {
+            while(sum > target){
                 sum -= arr[left];
                 left++;
             }
-
-            // Carry previous shortest length
-            if(right > 0)
-                best[right] = best[right - 1];
-
-            // Found a subarray
-            if(sum == target) {
-
+            if(right > 0) best[right] = best[right - 1];
+            if(sum == target){
                 int len = right - left + 1;
-
-                // Find another subarray BEFORE this one
-                if(left > 0 && best[left - 1] != INT_MAX) {
-
-                    answer = min(
-                        answer,
-                        len + best[left - 1]
-                    );
+                if(left > 0 && best[left - 1] != INT_MAX){
+                    ans = min(ans, len + best[left - 1]);
                 }
-
-                // Store current subarray if it is shorter
                 best[right] = min(best[right], len);
             }
         }
-
-        if(answer == INT_MAX)
-            return -1;
-
-        return answer;
+        if(ans == INT_MAX) return -1;
+        return ans;
     }
 };
