0e8df1cc4706b5346d5f72f3fb6b8280b107cf19
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-24 20:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (94.91%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(int i : prices) pq.push(i);
-        int a = pq.top();
-        pq.pop();
-        int b = pq.top();
-        pq.pop();
+        int a = INT_MAX;
+        int b = INT_MAX;
+        for(int price : prices){
+            if(price < a){
+                b = a;
+                a = price;
+            }
+            else if(price < b) b = price; 
+        }
         int ans = a + b;
         if(ans <= money) return money - ans;
         return money;
+
+
+        // priority_queue<int, vector<int>, greater<int>> pq;
+        // for(int i : prices) pq.push(i);
+        // int a = pq.top();
+        // pq.pop();
+        // int b = pq.top();
+        // pq.pop();
+        // int ans = a + b;
+        // if(ans <= money) return money - ans;
+        // return money;
 
 
 
