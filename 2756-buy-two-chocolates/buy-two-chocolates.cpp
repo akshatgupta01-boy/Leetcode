d26693e48d9f0b42d8d4dfcfc5d4199d7c9d839a
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-24 20:30:48 +0530

    Time: 10 ms (2.66%) | Memory: 52.7 MB (2.80%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-        int ans = prices[0] + prices[1];
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i : prices) pq.push(i);
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        int ans = a + b;
         if(ans <= money) return money - ans;
         return money;
+
+
+
+        // sort(prices.begin(), prices.end());
+        // int ans = prices[0] + prices[1];
+        // if(ans <= money) return money - ans;
+        // return money;
     }
 };
