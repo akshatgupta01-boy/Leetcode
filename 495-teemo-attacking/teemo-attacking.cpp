b90c9b59e6880c6878490bef6b545d7b5deb8871
Akshat Gupta <akshatgupta151593@gmail.com>
2026-09-25 17:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (63.24%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.cpp
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        int ans = 0;
         int n = timeSeries.size();
+        int ans = 0;
         for(int i=0;i<n-1;i++){
-            ans += min(duration, timeSeries[i+1] - timeSeries[i]);
+            ans += min(duration, timeSeries[i + 1] - timeSeries[i]);
         }
         ans += duration;
         return ans;
